0b3fbac173aa7461beae1cb7e2fec491226aa798
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2020-12-16 21:34:05 +0900

    feat: add files

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
