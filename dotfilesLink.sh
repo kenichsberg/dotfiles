eb2a5041c234cab86b78c8b71d552e780823eab4
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2020-12-24 18:57:31 +0900

    fix: make directories if there are not

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config/alacritty
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
