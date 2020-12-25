cb802d27cdb8d0463fbd18523df6e0642995c825
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2020-12-25 10:09:44 +0900

    fix: make dirs if there are not

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+mkdir -p ~/.vim/tmp/swap
+mkdir -p ~/.vim/tmp/backup
+mkdir -p ~/.vim/tmp/undo
+
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
